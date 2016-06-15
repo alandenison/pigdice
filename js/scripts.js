dbb860cbf71ac5cf9d2128fa528554c5cf4ddbee
Alan Denison and George Olson <alan@>
2016-06-15 10:52:15 -0700

    allow a second set of buttons for player 2 which updates their score

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,67 @@
 
 
 
-$(document).ready(function(){
+
   var player1Score = "0";
+  var player2Score = "0";
   var turnTotal = [0];
- $("#btn-roll").click(function(){
 
-   var roll = parseInt(Math.floor((Math.random() * 6) + 1));
-   $("#roll-result").append(roll + ", ");
-   if(roll != 1) {
-   var newTotal = parseInt(turnTotal) + roll;
-   turnTotal = newTotal;
-   $("#turn-total").text(turnTotal);
- } else {
-   turnTotal = 0;
-   $("#turn-total").text(turnTotal);
-   $("#roll-result").text("YOU LOSE");
- }
+  var rollDice = function(roll) {
+   roll = parseInt(Math.floor((Math.random() * 6) + 1));
+   return roll;
+  };
 
- });
- $("#btn-hold").click(function(){
-   player1Score = turnTotal+ parseInt(player1Score);
-   turnTotal = 0
-   $("#roll-result").text("");
-   $("#turn-total").text("");
-   $("#Player1").text(player1Score);
- })
+
+
+$(document).ready(function(){
+  $("#btn-roll1").click(function(){
+    rollResult = rollDice();
+    $("#roll-result").append(rollResult + ", ");
+    if(rollResult != 1) {
+    var newTotal = parseInt(turnTotal) + rollResult;
+    turnTotal = newTotal;
+    $("#turn-total").text(turnTotal);
+    } else {
+    turnTotal = 0;
+    $("#turn-total").text(turnTotal);
+    $("#roll-result").text("YOU LOSE");
+    }
+
+  });
+
+  $("#btn-hold1").click(function(){
+    player1Score = turnTotal+ parseInt(player1Score);
+    turnTotal = 0
+    $("#roll-result").text("");
+    $("#turn-total").text("");
+    $("#Player1").text(player1Score);
+    $("#player1Buttons").hide();
+    $("#player2Buttons").show();
+
+  });
+  $("#btn-roll2").click(function(){
+    rollResult = rollDice();
+    $("#roll-result").append(rollResult + ", ");
+    if(rollResult != 1) {
+    var newTotal = parseInt(turnTotal) + rollResult;
+    turnTotal = newTotal;
+    $("#turn-total").text(turnTotal);
+    } else {
+    turnTotal = 0;
+    $("#turn-total").text(turnTotal);
+    $("#roll-result").text("YOU LOSE");
+
+    }
+
+  });
+
+  $("#btn-hold2").click(function(){
+    player2Score = turnTotal+ parseInt(player2Score);
+    turnTotal = 0
+    $("#roll-result").text("");
+    $("#turn-total").text("");
+    $("#Player2").text(player2Score);
+    $("#player2Buttons").hide();
+    $("#player1Buttons").show();
+  });
 });
