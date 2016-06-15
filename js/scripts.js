a2b6480c144eb6008ae7d3908f5194dc58aed114
Alan Denison and George Olson <george@>
2016-06-15 15:14:47 -0700

    hide radio buttons and fix bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,22 @@
 
 
 
-
+  var multiDice = "";
   var player1Score = "0";
   var player2Score = "0";
   var turnTotal = [0];
   var oneArray = ["false"];
   // var one = "false";
-  var reset = "false";
+  resetArray = ["false"];
   rollArray = [];
 
 
-  // var rollDice = function(roll) {
-  //   if (multiDice === "1dice") {
-  //   roll = parseInt(Math.floor((Math.random() * 6) + 1));
-  //   return roll;
-  // } else {
-  //   roll = parseInt(Math.floor((Math.random() * 6) + 1))
-  //
-  // }
-  // };
   var rollDice = function(roll) {
     roll = parseInt(Math.floor((Math.random() * 6) + 1));
+    if (roll === 1 ){
+      oneArray.unshift("true");
+      console.log("oneArray: " + oneArray[0] + ". Also the full array is:" + oneArray);
+    }
     return roll;
   };
 
@@ -32,13 +27,14 @@
     roll2 = parseInt(Math.floor((Math.random() * 6) + 1));
     console.log(roll2);
     roll = roll1+roll2;
-    if (roll1 === 1 || roll2 === 1){
+
+    if (roll1 === 1 && roll2 === 1) {
+    resetArray.unshift("true");
+    console.log("Reset: " + resetArray[0]);
+    }
+    else if (roll1 === 1 || roll2 === 1){
     oneArray.unshift("true");
     console.log("oneArray: " + oneArray[0] + ". Also the full array is:" + oneArray);
-    }
-    else if (roll1 === 1 && roll2 === 1) {
-    var reset = "true";
-    console.log("Reset: " + reset);
     }
     return roll;
   };
@@ -51,13 +47,24 @@
 $(document).ready(function(){
   $("#btn-roll1").click(function(){
     $(".lose").remove();
-    var multiDice = $("input:radio[name=diceValue]:checked").val();
+    $("#diceRadio").fadeOut(100);
+    multiDice = $("input:radio[name=diceValue]:checked").val();
+    console.log(multiDice);
     if (multiDice === "1dice"){
-      $("#roll-result").text(" ");
+      // $("#roll-result").text(" ");
       rollResult = rollDice();
+      $("#roll-result").append(rollResult + ", ");
       } else {
       rollResult = rollDice2();
       $("#roll-result").append(rollResult + ", ");
+      }
+      if(resetArray[0] === "true"){
+        player1Score = 0;
+        $("#Player1").text(player1Score);
+        alert("BUAHHAHAHHAH");
+        $("#player1Buttons").hide();
+        $("#player2Buttons").show();
+        resetArray.shift();
       }
     if(oneArray[0] === "true") {
       turnTotal = 0;
@@ -77,11 +84,14 @@ $(document).ready(function(){
   });
 
   $("#btn-hold1").click(function(){
-    player1Score = turnTotal+ parseInt(player1Score);
+    player1Score = turnTotal + parseInt(player1Score);
     turnTotal = 0
-    if (player1Score >=100) {
+    if (player1Score >=100 && multiDice != "2dice") {
       alert(" PLAYER 1 WINNER ");
-    } else {
+    } else if (player1Score >= 200){
+      alert("PLAYER 1 WINNER");
+    }
+      else {
     $("#roll-result").text("");
     $("#turn-total").text("");
     $("#Player1").text(player1Score);
@@ -92,13 +102,21 @@ $(document).ready(function(){
   });
   $("#btn-roll2").click(function(){
     $(".lose").remove();
-    var multiDice = $("input:radio[name=diceValue]:checked").val();
     if (multiDice === "1dice"){
-      $("#roll-result").text(" ");
+      // $("#roll-result").text(" ");
       rollResult = rollDice();
+      $("#roll-result").append(rollResult + ", ");
       } else {
       rollResult = rollDice2();
       $("#roll-result").append(rollResult + ", ");
+      }
+      if(resetArray[0] === "true"){
+        player2Score = 0;
+        $("#Player2").text(player2Score);
+        alert("BUAHHAHAHHAH");
+        $("#player2Buttons").hide();
+        $("#player1Buttons").show();
+        resetArray.shift();
       }
       if(oneArray[0] === "true") {
         turnTotal = 0;
@@ -119,11 +137,14 @@ $(document).ready(function(){
     });
 
   $("#btn-hold2").click(function(){
-    player2Score = turnTotal+ parseInt(player2Score);
+    player2Score = turnTotal + parseInt(player2Score);
     turnTotal = 0
-    if (player2Score >=100) {
+    if (player2Score >=100 && multiDice != "2dice") {
       alert(" PLAYER 2 WINNER ");
-    } else {
+    } else if (player2Score >= 200){
+      alert("PLAYER 2 WINNER");
+    }
+      else {
     $("#roll-result").text("");
     $("#turn-total").text("");
     $("#Player2").text(player2Score);
