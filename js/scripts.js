690ffa493897d43a3007a9c06a5c671430b02903
Alan Denison and George Olson <george@>
2016-06-15 10:01:44 -0700

    reset turn score to 0 when you roll a 1

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,15 @@ $(document).ready(function(){
 
    var roll = parseInt(Math.floor((Math.random() * 6) + 1));
    $("#roll-result").append(roll + ", ");
+   if(roll != 1) {
    var newTotal = parseInt(turnTotal) + roll;
    turnTotal = newTotal;
    $("#turn-total").text(turnTotal);
+ } else {
+   turnTotal = 0;
+   alert(turnTotal);
+   $("#turn-total").text(turnTotal);
+ }
 
 
 
