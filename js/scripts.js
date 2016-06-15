91c25900fcd11e1b52d333dc8c1d12d08d119bf6
Alan Denison and George Olson <george@>
2016-06-15 09:52:57 -0700

    keep a tally of the running turn total

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,17 @@
-// function runningTotal(base, roll) {
-//  this.base = base;
-//  this.roll = roll;
-// };
+
 
 
 $(document).ready(function(){
+  var turnTotal = [0];
  $("#btn-roll").click(function(){
 
-   var roll = Math.floor((Math.random() * 6) + 1);
-   $("#roll-result").text(roll);
+   var roll = parseInt(Math.floor((Math.random() * 6) + 1));
+   $("#roll-result").append(roll + ", ");
+   var newTotal = parseInt(turnTotal) + roll;
+   turnTotal = newTotal;
+   $("#turn-total").text(turnTotal);
+
+
 
  });
 });
