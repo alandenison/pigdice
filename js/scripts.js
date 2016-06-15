eda9383a616616b9853f49e2e1f507bc266fd9ee
Alan Denison and George Olson <george@>
2016-06-15 11:03:24 -0700

    end the game when a players score goes to or over 100

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,7 @@
 
 $(document).ready(function(){
   $("#btn-roll1").click(function(){
+    $("#roll-result").text("");
     rollResult = rollDice();
     $("#roll-result").append(rollResult + ", ");
     if(rollResult != 1) {
@@ -25,6 +26,8 @@ $(document).ready(function(){
     turnTotal = 0;
     $("#turn-total").text(turnTotal);
     $("#roll-result").text("YOU LOSE");
+    $("#player1Buttons").hide();
+    $("#player2Buttons").show();
     }
 
   });
@@ -32,14 +35,19 @@ $(document).ready(function(){
   $("#btn-hold1").click(function(){
     player1Score = turnTotal+ parseInt(player1Score);
     turnTotal = 0
+    if (player1Score >=100) {
+      alert(" PLAYER 1 WINNER ");
+    } else {
     $("#roll-result").text("");
     $("#turn-total").text("");
     $("#Player1").text(player1Score);
     $("#player1Buttons").hide();
     $("#player2Buttons").show();
+    }
 
   });
   $("#btn-roll2").click(function(){
+    $("#roll-result").text("");
     rollResult = rollDice();
     $("#roll-result").append(rollResult + ", ");
     if(rollResult != 1) {
@@ -50,6 +58,8 @@ $(document).ready(function(){
     turnTotal = 0;
     $("#turn-total").text(turnTotal);
     $("#roll-result").text("YOU LOSE");
+    $("#player2Buttons").hide();
+    $("#player1Buttons").show();
 
     }
 
@@ -58,10 +68,14 @@ $(document).ready(function(){
   $("#btn-hold2").click(function(){
     player2Score = turnTotal+ parseInt(player2Score);
     turnTotal = 0
+    if (player2Score >=100) {
+      alert(" PLAYER 2 WINNER ");
+    } else {
     $("#roll-result").text("");
     $("#turn-total").text("");
     $("#Player2").text(player2Score);
     $("#player2Buttons").hide();
     $("#player1Buttons").show();
+    }
   });
 });
