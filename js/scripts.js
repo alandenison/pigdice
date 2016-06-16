ec2820befad4c00ff838c654ef80a8ab6831516d
Alan Denison and George Olson <george@>
2016-06-16 16:16:29 -0700

    Add initial page with form to input player names, number of dice, highest total to win, and iconic pig avatar. Include animation that randomizes dice face images to simulate rolling of the dice.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-
+function Record(nameOne, player1Score, nameTwo, player2Score) {
+  this.nameOne = nameOne;
+  this.player1Score = player1Score;
+  this.nameTwo = nameTwo;
+  this.player2Score = player2Score;
+}
 
 
   var multiDice = "";
@@ -9,10 +14,28 @@
   // var one = "false";
   resetArray = ["false"];
   rollArray = [];
+  playerIds = []
+  maxScore = [];
+  avatarArray = [];
 
 
   var rollDice = function(roll) {
+    $(".dicePics").hide();
+    $("#playerButtons .btn").hide().show(2700);
+
+    for(i=0; i<=6; i++) {
+
+      var diAnimate = "#dic" + parseInt(Math.floor((Math.random() * 6) + 1));
+      $(diAnimate).delay(i*100).show(99).hide(1);
+
+      console.log(diAnimate);
+    };
+
     roll = parseInt(Math.floor((Math.random() * 6) + 1));
+    // roll = 100;
+    rollArray.unshift(roll);
+    var idForDice = "#di" + roll;
+    $(idForDice).delay(1500).fadeIn(1);
     if (roll === 1 ){
       oneArray.unshift("true");
       console.log("oneArray: " + oneArray[0] + ". Also the full array is:" + oneArray);
@@ -21,24 +44,42 @@
   };
 
   var rollDice2 = function(roll1, roll2) {
+    $(".dicePics").hide();
+    $("#dubs").text("");
+    $("#playerButtons .btn").hide().show(2700);
     var one = "false";
+    for(i=0; i<=6; i++) {
+
+      var diAnimate = "#dic" + parseInt(Math.floor((Math.random() * 6) + 1));
+      $(diAnimate).delay(i*100).show(99).hide(1);
+     }
     roll1 = parseInt(Math.floor((Math.random() * 6) + 1));
     console.log(roll1);
     roll2 = parseInt(Math.floor((Math.random() * 6) + 1));
     console.log(roll2);
+
+
     roll = roll1+roll2;
 
+    var idForDice1 = "#di" + roll1;
+    var idForDice2 = "#di" + roll2;
+    if (idForDice1 === idForDice2) {
+      $("#dubs").append('<img src="img/die_face_' + roll2 + '.png"/>');
+      $("#dubs").delay(1500).show(1);
+      $(idForDice1).delay(1500).fadeIn(1);
+    } else {
+
+    $(idForDice1).delay(1500).fadeIn(1);
+    $(idForDice2).delay(1500).fadeIn(1);
+    }
     if (roll1 === 1 && roll2 === 1) {
     resetArray.unshift("true");
-    console.log("Reset: " + resetArray[0]);
     }
     else if (roll1 === 1 || roll2 === 1){
     oneArray.unshift("true");
-    console.log("oneArray: " + oneArray[0] + ". Also the full array is:" + oneArray);
     }
     return roll;
   };
-
 
 
 
@@ -49,14 +90,35 @@ $(document).ready(function(){
     $("#amount-dice").text("1 di");
     $("#rules2").fadeOut();
     $("#rules1").fadeIn();
+    $("#beginButton").fadeIn();
   });
   $("#2dice").click(function(){
     $("#amount-dice").text("2 dice");
     $("#rules1").fadeOut();
     $("#rules2").fadeIn();
-
+    $("#beginButton").fadeIn();
   });
 
+  $("form#player-names").submit(function(event){
+    event.preventDefault();
+
+    var nameOne = $("#player1-name").val();
+    var nameTwo = $("#player2-name").val();
+    maxScore.push(parseInt($("#startingScore").val()));
+    avatarArray.push($("#avatar").val());
+    avatarArray.push($("#avatar2").val());
+    $("#btn-roll1").append("<img src=" + avatarArray[0] + "></img>");
+    $("#btn-roll2").append("<img src=" + avatarArray[1] + "></img>");
+    $(".player-one").text(nameOne);
+    $(".player-two").text(nameTwo);
+    playerIds.push(nameOne, nameTwo);
+    $("#player-names").fadeOut();
+    $("#game-page").fadeIn();
+    $(".rules-die").fadeOut();
+    $("#avatarDiv").hide();
+  });
+
+//PLAYER 1 ROLL
   $("#btn-roll1").click(function(){
     $(".lose").remove();
     $("#diceRadio").fadeOut(100);
@@ -101,30 +163,36 @@ $(document).ready(function(){
     }
 
   });
-
+// PLAYER 1 HOLD BUTTON
   $("#btn-hold1").click(function(){
     player1Score = turnTotal + parseInt(player1Score);
     turnTotal = 0
-    if (player1Score >=100 && multiDice != "2dice") {
+    if (player1Score >=maxScore[0]) {
+      var nameOne = playerIds[0];
+      var nameTwo = playerIds[1];
+      var gameRecord = new Record(nameOne, player1Score, nameTwo, player2Score);
+      console.log("testing gameRecord object " + gameRecord.nameOne);
+      $("#gameTracker").append('<li>' + gameRecord.nameOne + ', Won with a score of: ' + gameRecord.player1Score + '. ' + gameRecord.nameTwo + ', SCORE: ' + gameRecord.player2Score);
       alert(" PLAYER 1 WINNER ");
+      rollResult = 0;
+      player1Score = 0;
+      player2Score = 0;
+      $("#roll-result").text("");
+      $("#turn-total").text("");
       $("#winPig").show();
       $("#winPig").fadeOut(5000);
-    } else if (player1Score >= 200){
-      $("#winPig").show();
-      $("#winPig").fadeOut(5000);
-      alert("PLAYER 1 WINNER");
-    }
-      else {
-    $("#roll-result").text("");
-    $("#turn-total").text("");
-    $("#Player1").text(player1Score);
-    $("#player1Buttons").fadeOut(00);
-    $("#indicator").fadeIn(100);
-    $("#indicator").fadeOut(1500);
-    $("#player2Buttons").fadeIn(2000);
-    }
+      } else {
+        $("#roll-result").text("");
+        $("#turn-total").text("");
+        $("#Player1").text(player1Score);
+        $("#player1Buttons").fadeOut(00);
+        $("#indicator").fadeIn(100);
+        $("#indicator").fadeOut(1500);
+        $("#player2Buttons").fadeIn(2000);
+      }
 
   });
+  //PLAYER 2 ROLL
   $("#btn-roll2").click(function(){
     $(".lose").remove();
     if (multiDice === "1dice"){
@@ -167,20 +235,25 @@ $(document).ready(function(){
       }
 
     });
-
+//PLAYER 2 HOLD
   $("#btn-hold2").click(function(){
     player2Score = turnTotal + parseInt(player2Score);
     turnTotal = 0
-    if (player2Score >=100 && multiDice != "2dice") {
+    if (player2Score >=maxScore[0]) {
       $("#winPig").show();
       $("#winPig").fadeOut(5000);
+      var nameOne = playerIds[0];
+      var nameTwo = playerIds[1];
+      var gameRecord = new Record(nameOne, player1Score, nameTwo, player2Score);
+      $("#gameTracker").append('<li>' + gameRecord.nameOne + ', SCORE: ' + gameRecord.player1Score + '. ' + gameRecord.nameTwo + ', WON with a score of: ' + gameRecord.player2Score);
       alert(" PLAYER 2 WINNER ");
-    } else if (player2Score >= 200){
-      alert("PLAYER 2 WINNER");
-      $("#winPig").show();
-      $("#winPig").fadeOut(5000);
-    }
-      else {
+      rollResult = 0;
+      player1Score = 0;
+      player2Score = 0;
+      $("#roll-result").text("");
+      $("#turn-total").text("");
+      $("#diceFaces").hide();
+    } else {
     $("#roll-result").text("");
     $("#turn-total").text("");
     $("#Player2").text(player2Score);
